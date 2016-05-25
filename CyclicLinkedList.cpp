7477354d9a80c4928843056c1f1e2d01e3d1d55b
RandomGuy <randomguy@localhost.localdomain>
2016-05-24 21:36:48 -0400

    cycliclinkedlist class updated

--- CyclicLinkedList.cpp
+++ CyclicLinkedList.cpp
@@ -19,3 +19,65 @@ CyclicLinkedList::~CyclicLinkedList() {
     delete tmp;
     head = NULL;
 }
+
+int CyclicLinkedList::getSize() const {
+    return size;
+}
+
+bool CyclicLinkedList::empty() const {
+    return size == 0;
+}
+
+template<typename Type>
+Type CyclicLinkedList::front() const {
+    return head;
+}
+
+template<typename Type>
+Type CyclicLinkedList::back() const {
+    return tail;
+}
+
+template<typename Type>
+SingleNode<Type>* CyclicLinkedList::getHead() const {
+    return head->data;
+}
+
+template<typename Type>
+SingleNode<Type>* CyclicLinkedList::getTail() const {
+    return tail->data;
+}
+
+template<typename Type>
+int CyclicLinkedList::getCount(Type const &arg) const {
+    int count = 0;
+    SingleNode * ptr = head;
+
+    while(ptr != tail){
+        if(ptr->data == arg){
+            count++;
+        }
+    }
+    if(tail->data == arg)
+        count++;
+
+    return count;
+}
+
+template<typename Type>
+void CyclicLinkedList::push_front(Type const &newNode) {
+    SingleNode * newPtr = new SingleNode(newNode, head);
+    head = newPtr;
+    size++;
+}
+
+template<typename Type>
+void CyclicLinkedList::push_back(Type const &newNode) {
+    SingleNode * newPtr = new SingleNode(newNode, head);
+    tail->next = newPtr;
+    tail = newPtr;
+    size++;
+}
+
+
+
